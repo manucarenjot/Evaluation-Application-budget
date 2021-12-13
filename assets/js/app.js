d6551523679c8cedbbf6b870a8b1951136ba0a84
manucarenjot <carenjotje@gmail.com>
2021-12-13 15:11:32 +0100

    Eval

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+let depenseFixe = document.getElementById('depenseFixe');
+let loyerCharge = document.getElementById('loyerCharges');
+let credit = document.getElementById('credit');
+let eauGazElec = document.getElementById('eauGazElec');
+let telNet = document.getElementById('telNet');
+let assurHabitat = document.getElementById('assurHabita');
+let assurVehic = document.getElementById('assuVehic');
+let mutuel = document.getElementById('mutuel');
+let fraisGarde = document.getElementById('fraisGarde');
+let impotRev = document.getElementById('impotRev');
+let impotLocaux = document.getElementById('impotLocaux');
+let depenseCourantes = document.getElementById('depenseCourantes');
+let courses = document.getElementById('courses');
+let essence = document.getElementById('essence');
+let sport = document.getElementById('sport');
+let depenseOcas = document.getElementById('depenseOcas');
+let sortie = document.getElementById('sorties');
+let autre = document.getElementById('autre');
+
+
